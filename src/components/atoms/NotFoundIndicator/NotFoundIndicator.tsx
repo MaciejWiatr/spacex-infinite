60a00f343635081bdb085c7e016acdd7e29b5b46
Maciej Wiatr <maciekwiatr17@gmail.com>
2022-04-03 16:25:59 +0200

    Improve text visibility

--- src/components/atoms/NotFoundIndicator/NotFoundIndicator.tsx
+++ src/components/atoms/NotFoundIndicator/NotFoundIndicator.tsx
@@ -1,3 +1,5 @@
 export const NotFoundIndicator = () => {
-  return <div className="p-4">Nothing was found</div>
+  return (
+    <div className="p-4 font-semibold text-gray-500">Nothing was found</div>
+  )
 }
